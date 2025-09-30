4768d22867025fb592db327cacbddda06fcb30de
Angello Gaitan <agaitan@magickhub.com>
2025-09-30 13:08:33 -0500

    ajuste editores de codigo

--- src/app/shared/components/code-editor/code-editor.component.ts
+++ src/app/shared/components/code-editor/code-editor.component.ts
@@ -1,39 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AceModule } from 'ngx-ace-wrapper';
 
+import { Component, ElementRef, Input, AfterViewInit, ViewChild } from '@angular/core';
+import { NgClass } from '@angular/common';
 import 'ace-builds/src-noconflict/ace';
 import 'ace-builds/src-noconflict/mode-scheme';
 import 'ace-builds/src-noconflict/mode-ocaml';
-import 'ace-builds/src-noconflict/theme-github';
+
+declare const ace: any;
 
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.css'],
   standalone: true,
-  imports: [AceModule]
+  imports: [NgClass]
 })
-
-export class CodeEditorComponent implements OnInit {
+export class CodeEditorComponent implements AfterViewInit {
   @Input() code: string = '';
   @Input() language: string = 'text';
   @Input() label: string = '';
   @Input() disabled: boolean = true;
+  @ViewChild('editor') editorRef!: ElementRef;
+  private editor: any;
 
-  constructor() { }
-
-  ngOnInit() {}
-
-  onAceLoaded(editor: any) {
-    // Solo se usan los modos y tema importados estáticamente
+  ngAfterViewInit() {
+    this.editor = ace.edit(this.editorRef.nativeElement);
+    this.editor.setTheme('ace/theme/github');
     const mode = this.language === 'scheme' || this.language === 'ocaml' ? this.language : 'text';
-  const theme = 'github';
-    // Desactiva el worker para evitar errores
-    editor.session.setOption('useWorker', false);
-    // Establece el modo y tema
-    editor.session.setMode(`ace/mode/${mode}`);
-    editor.setTheme(`ace/theme/${theme}`);
-    // Solo lectura si disabled
-    editor.setReadOnly(this.disabled);
+    this.editor.session.setMode(`ace/mode/${mode}`);
+    this.editor.setValue(this.code || '', -1);
+    this.editor.setReadOnly(this.disabled);
+    this.editor.session.setOption('useWorker', false);
+    this.editor.on('change', () => {
+      this.code = this.editor.getValue();
+    });
+  }
+
+  ngOnChanges() {
+    if (this.editor) {
+      this.editor.setReadOnly(this.disabled);
+      this.editor.setValue(this.code || '', -1);
+      const mode = this.language === 'scheme' || this.language === 'ocaml' ? this.language : 'text';
+      this.editor.session.setMode(`ace/mode/${mode}`);
+    }
   }
 }
